fa42ba492d05a7aea02a94bce1bc092f524797ff
LucasMazza42 <mazzalucas42@gmail.com>
2023-07-16 11:10:00 -0600

    menu update

--- app.js
+++ app.js
@@ -102,3 +102,20 @@ if (startButton) {
 if (endButton) {
     endButton.addEventListener('click', resetTime);
 }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
